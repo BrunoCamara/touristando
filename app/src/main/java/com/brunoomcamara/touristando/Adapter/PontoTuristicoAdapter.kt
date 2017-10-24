a74d87c87278cdf7f77e3a55a779a7ad5855e040
Kleber Faustino <faustikle@gmail.com>
2017-10-24 00:35:00 -0300

    Implementado activity para PontoTuristico

---
+++ app/src/main/java/com/brunoomcamara/touristando/Adapter/PontoTuristicoAdapter.kt
@@ -0,0 +1,32 @@
+package com.brunoomcamara.touristando.Adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.brunoomcamara.touristando.Model.PontoTuristico
+import com.brunoomcamara.touristando.R
+import com.brunoomcamara.touristando.ViewHolder.PontoTuristicoHolder
+
+class PontoTuristicoAdapter(private val pontosTuristicos: MutableList<PontoTuristico>, val onClickCidade: (PontoTuristico)->Unit) : RecyclerView.Adapter<PontoTuristicoHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PontoTuristicoHolder {
+        return PontoTuristicoHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.recycler_line_text, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: PontoTuristicoHolder, position: Int) {
+        holder.titulo.text = pontosTuristicos.get(position).nome
+        holder.titulo.setOnClickListener({onClickCidade(pontosTuristicos.get(position))})
+    }
+
+    override fun getItemCount(): Int = pontosTuristicos.size
+
+    fun addPontos(pontosTuristicos: List<PontoTuristico>) {
+        for (pontoTuristico in pontosTuristicos) {
+            this.pontosTuristicos.add(pontoTuristico)
+        }
+
+        notifyItemInserted(getItemCount())
+        notifyItemRangeInserted(getItemCount(), pontosTuristicos.size)
+    }
+}
